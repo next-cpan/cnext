77331e2c3cdeb0b3bf7355ca981622285ac97a7c
Nicolas R <cpan@atoomic.org>
2020-04-07 11:12:00 -0600

    cleanup tarball path

--- lib/App/cplay/cmd/fromtarball.pm
+++ lib/App/cplay/cmd/fromtarball.pm
@@ -6,6 +6,7 @@ use App::cplay::Logger;    # import all
 use App::cplay::Installer;
 
 use File::pushd;
+use File::Path ();
 
 use Cwd;
 
@@ -25,11 +26,15 @@ sub run ( $cli, @args ) {
 
     DEBUG("tarball is extracted at $path");
 
-    my $in_dir = pushd($path) or return 1;
-    return 1 unless my $BUILD = $installer->load_BUILD_json();
+    {
+        my $in_dir = pushd($path) or return 1;
+        return 1 unless my $BUILD = $installer->load_BUILD_json();
 
-    $installer->depth(1);    # need to setup depth
-    $installer->install_from_BUILD($BUILD);
+        $installer->depth(1);    # need to setup depth
+        $installer->install_from_BUILD($BUILD);
+    }
+
+    File::Path::rmtree($path);
 
     DONE("install fromtarball succeeds");
 
@@ -38,12 +43,13 @@ sub run ( $cli, @args ) {
 
 sub setup_tarball ( $installer, $tarball_or_url ) {
 
-    my $tarball = download_if_needed( $installer, $tarball_or_url );
+    my ( $tarball, $has_downloaded ) = download_if_needed( $installer, $tarball_or_url );
     return unless defined $tarball && -f $tarball;
     DEBUG("Using tarball from $tarball");
 
     my $relative_path = $installer->unpacker->unpack($tarball);
     my $full_path     = $installer->cli->build_dir . '/' . $relative_path;
+    unlink($tarball) if $has_downloaded;
     if ( !defined $relative_path || !-d $full_path ) {
         FAIL("fail to extract tarball $tarball");
         return;
@@ -66,7 +72,7 @@ sub download_if_needed ( $installer, $tarball_or_url ) {
     App::cplay::Logger::fetch($tarball_or_url);
     $installer->cli->http->mirror( $tarball_or_url, $path );
 
-    return $path;
+    return ( $path, 1 );
 }
 
 1;
