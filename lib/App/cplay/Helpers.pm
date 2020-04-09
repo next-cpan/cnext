419839e88aa133f01b0cbcf2445bf4ce67c80e87
Nicolas R <cpan@atoomic.org>
2020-04-09 16:08:56 -0600

    add update_shebang helper

--- lib/App/cplay/Helpers.pm
+++ lib/App/cplay/Helpers.pm
@@ -7,7 +7,7 @@ use File::Which ();
 use App::cplay::Logger;
 
 use Exporter 'import';
-our @EXPORT_OK = qw(read_file zip write_file is_fatpacked);
+our @EXPORT_OK = qw(read_file zip write_file is_fatpacked update_shebang);
 
 sub read_file ( $file, $mode = ':utf8' ) {
     local $/;
@@ -80,6 +80,28 @@ sub prove_binary {
     }
 
     FATAL("Cannot find 'prove' binary");
+}
+
+sub update_shebang($file) {
+    return unless defined $file && -f $file;
+
+    open( my $input, '<', $file )
+      or die "Fail to open file: $! " . join( ' ', ( caller(1) )[ 0, 1, 2, 3 ] ) . "\n";
+
+    my $output_file = "$file.tmp";
+    open( my $output, '>', $output_file )
+      or die "Fail to open file: $! " . join( ' ', ( caller(1) )[ 0, 1, 2, 3 ] ) . "\n";
+
+    my $c = 0;
+    while ( my $line = <$input> ) {
+        if ( ++$c == 1 && $line =~ m{^#!.*perl} ) {
+            $line = qq[#!$^X];    # update the shebang line
+        }
+
+        print {$output} $line;
+    }
+
+    return;
 }
 
 1;
